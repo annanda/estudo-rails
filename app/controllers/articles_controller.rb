9a9e3ac5e5e87de3ea452da0e562edb7a0c6e7c9
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-08 11:47:26 -0300

    Adicionando a action show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,9 @@ class ArticlesController < ApplicationController
         redirect_to @article
     end
 
+    def show
+    end
+
     private
     def articles_params
         params.require(:article).permit(:title, :text)
