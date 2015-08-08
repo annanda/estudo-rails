d23f8e8f8392d609dcdf19dce6a88da43c9e585f
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-08 11:40:41 -0300

    Fazendo o action create salvar os dados do form no banco de dados

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,9 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(params.require(:article).permit(:title, :text))
+
+        @article.save
+        redirect_to @article
     end
 end
