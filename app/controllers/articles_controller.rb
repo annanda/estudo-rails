e32ffb656c1466e1f96bca012bfa760584df9b93
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-08 11:58:06 -0300

    Criando e exibindo novos articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
     end
 
@@ -7,9 +11,6 @@ class ArticlesController < ApplicationController
 
         @article.save
         redirect_to @article
-    end
-
-    def show
     end
 
     private
