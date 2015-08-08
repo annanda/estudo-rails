6a33b2d6c73391b1d3e127b65d08e53f46f279bb
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-08 10:33:08 -0300

    Criando um novo resources :articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+  resources :articles
+  root 'welcome#index'
+
+end
+
+  # get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +60,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
