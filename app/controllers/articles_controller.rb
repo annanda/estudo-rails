6b6a9bfcf2e61485807b99b02012d5062c8c5d0c
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-09 18:33:11 -0300

    Adicionando a action update e os links para editar e mostrar na index e no show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(articles_params)
 
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
             redirect_to @article
         else
             render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(articles_params)
+            reditect_to @article
+        else
+            render 'edit'
         end
     end
 
