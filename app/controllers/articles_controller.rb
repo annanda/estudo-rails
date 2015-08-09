f8b7f28ac4aed199de264290f1d07b746e34b504
Annanda Sousa <annanda.sousa@gmail.com>
2015-08-09 14:56:42 -0300

    Adicionando validação no titulo do form de criar um artigo.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,17 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
     end
 
     def create
         @article = Article.new(articles_params)
 
-        @article.save
-        redirect_to @article
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
 
     private
